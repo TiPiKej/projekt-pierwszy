acff0c56ded6e74af3c29e62d6e745e997a4e3b1
Tomasz <romukkemot@gmail.com>
2021-08-21 22:21:48 +0200

    save location on address bar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 import InputsComp from './views/Inputs'
 import ListComp from './views/List'
 import NavComp from './components/Nav'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [view, setView] = useState(0);
   const [res, setRes] = useState({});
 
+  const handleChangeView = (vNbr = 0) => {
+    if (vNbr < 0 || viewsList.length <= vNbr) return;
+
+    setView(vNbr)
+
+    const url = new URL(window.location.href);
+    url.searchParams.set('l', vNbr);
+    window.history.pushState({}, '', url);
+  }
+
+  useEffect(() => {
+    const parameters = new URL(window.location.href).searchParams
+
+    const curLoc = Number(parameters.get('l'));
+
+    if (!isNaN(curLoc)) handleChangeView(curLoc)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   const viewsList = [
-    {v: <InputsComp view={view} setView={setView} setRes={setRes} />, navInfo: "Wpisz dane"},
+    {v: <InputsComp view={view} setView={handleChangeView} setRes={setRes} />, navInfo: "Wpisz dane"},
     {v: <ListComp res={res} />, navInfo: "Lista użytkowników"}];
 
   return (
     <div className="App">
       <NavComp
-        setView={setView}
+        setView={handleChangeView}
         viewsList={viewsList} />
       {viewsList[view].v}
     </div>
