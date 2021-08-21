87c0e687cd0ca177296cd187cee190502af9c306
Tomasz <romukkemot@gmail.com>
2021-08-21 20:24:55 +0200

    Update List.js

--- src/views/List.js
+++ src/views/List.js
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react";
 import { connect } from "react-redux";
+import UserBlock from "../components/UserBlock";
+
+const url = 'http://localhost:8080/api/getUsers';
 
 function List(props) {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        if (!res.ok) return;
+
+        if (props.userData.name === '' && props.userData.surname === '') {
+          setUsers(res.users);
+          return;
+        }
+
+        setUsers(res.users.filter(u => 
+          u.name === props.userData.name && u.surname === props.userData.surname
+        ));
+        
+      })
+
+  }, [props.userData]);
+
   return (
     <div className="List">
-      <p>{props.userData.name}</p>
-      <p>{props.userData.surname}</p>
-      <p>{props.userData.address}</p>
+      { users.map((user, i) => <UserBlock user={user} key={`${user.name}${i}`} />) }
     </div>
   );
 }
